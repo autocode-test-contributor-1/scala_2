3273683d0a74558dc9626b7597b4b7cf09d29374
Kirill Peshin <kirill_peshin@epam.com>
2021-07-04 17:42:26 +0300

    Upd

--- src/main/scala/com/epam/Calculator.scala
+++ src/main/scala/com/epam/Calculator.scala
@@ -1,5 +1,10 @@
 package com.epam
 
 object Calculator extends App {
-  def add(x: Int, y: Int): Int = x + y + 1
+  def add(x: Int, y: Int): Int =  {
+    var sum: Int = 0
+    var summ: Int = 0
+    sum = x + y + 1
+    return sum
+  }
 }
